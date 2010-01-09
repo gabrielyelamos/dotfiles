6aacf45be8a1aeb68c0de36c766a855792b86bb9
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-01-09 17:13:05 +0100

    Remove hardcoded vars

--- bash/configs/Proxy environment.sh
+++ bash/configs/Proxy environment.sh
@@ -1,8 +1,23 @@
+#!/usr/bin/env bash
+# ------------------------------------------------------------------------------
+# Description
+#   Propagate the proxy settings to the system and applications. The proxy
+#   variables must have been set:
+#     $ export PROXY_HOST="proxy"
+#     $ export PROXY_PORT="port"
+#     $ export NON_PROXY_HOSTS="host1|host2|..."
+#
+# Author
+#   nicoulaj - http://www.ju-n.net
+# 
+# Project
+#   dotfiles - http://github.com/nicoulaj/dotfiles
+# ------------------------------------------------------------------------------
+
 # Http proxy
 export http_proxy="http://${PROXY_HOST}:${PROXY_PORT}"
 
 # Execution options
-export JAVA_OPTS="${JAVA_OPTS} -Dhttp.proxyHost=${PROXY_HOST} -Dhttp.proxyPort=${PROXY_PORT} -Dhttp.proxySet=true -Dhttp.nonProxyHosts=localhost|*.frec.bull.fr"
-export MAVEN_OPTS="${MAVEN_OPTS} -Dhttp.proxyHost=${PROXY_HOST} -Dhttp.proxyPort=${PROXY_PORT} -Dhttp.proxySet=true -Dhttp.nonProxyHosts=localhost|*.frec.bull.fr"
-export ANT_OPTS="${ANT_OPTS} -Dhttp.proxyHost=${PROXY_HOST} -Dhttp.proxyPort=${PROXY_PORT} -Dhttp.proxySet=true"
-
+export JAVA_OPTS="${JAVA_OPTS} -Dhttp.proxyHost=${PROXY_HOST} -Dhttp.proxyPort=${PROXY_PORT} -Dhttp.proxySet=true -Dhttp.nonProxyHosts=${NON_PROXY_HOSTS}"
+export MAVEN_OPTS="${MAVEN_OPTS} -Dhttp.proxyHost=${PROXY_HOST} -Dhttp.proxyPort=${PROXY_PORT} -Dhttp.proxySet=true -Dhttp.nonProxyHosts=${NON_PROXY_HOSTS}"
+export ANT_OPTS="${ANT_OPTS} -Dhttp.proxyHost=${PROXY_HOST} -Dhttp.proxyPort=${PROXY_PORT} -Dhttp.proxySet=true -Dhttp.nonProxyHosts=${NON_PROXY_HOSTS}"
