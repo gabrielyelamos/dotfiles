cb334dbde5d0f948ec3b3bf61a897f9f432f644c
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-01-22 16:37:34 +0100

    Improve general settings

--- bash/configs/Settings.sh
+++ bash/configs/Settings.sh
@@ -14,12 +14,14 @@
 # ------------------------------------------------------------------------------
 
 
-# Use 'most' as pager
-export PAGER="/usr/bin/most"
+# Use 'most' as pager, default to 'less' if not available
+export PAGER=`which most`
+export PAGER=${PAGER:-less}
 
 
-# Use 'geany' as editor
-export EDITOR="/usr/bin/geany"
+# Use 'geany' as editor, default to gedit if not available
+export EDITOR=`which geany`
+export EDITOR=${EDITOR:-gedit}
 
 
 # Set displayed directories when running '$ cd <TAB>'
@@ -35,13 +37,26 @@ if [ -f /etc/bash_completion ]; then
     . /etc/bash_completion
 fi
 
+
 # Ignore duplicates in history control without overriding Midnight Commander's
 # setting for 'ignorespaces'
 export HISTCONTROL=$HISTCONTROL${HISTCONTROL+,}ignoredups
 
 
+# Don't keep useless stuff in history
+export HISTIGNORE="cd:ls:[bf]g:clear"
+
+
 # Append to the history file, don't overwrite it
 shopt -s histappend
+
+
+# Number of history lines per session
+export HISTSIZE=5000
+
+
+# Number of total history lines
+export HISTFILESIZE=20000
 
 
 # Check the window size after each command and, if necessary,
