2adbda7c4945187175d7814805c012b7c4169f40
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-02-20 18:37:19 +0100

    Add utilitary functions for Gnome

---
+++ bash/configs/Gnome utils.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# ------------------------------------------------------------------------------
+# Description
+#   Helper functions for Gnome.
+#
+# Dependencies
+#   * Commands: gconftool.
+#
+# Author
+#   nicoulaj - http://www.ju-n.net
+# 
+# Project
+#   dotfiles - http://github.com/nicoulaj/dotfiles
+# ------------------------------------------------------------------------------
+
+export GNOME_CONFIGS_LOCATION=~/projects/dotfiles/gnome
+
+# Load the given configuration for Gnome-panel.
+#
+# Arguments
+#   1 (required) the name of the config to load. The file will be looked
+#                up at $GNOME_CONFIGS_LOCATION/apps/panel/<name>.xml
+gnome-panel-load()
+{
+    gconftool --load $GNOME_CONFIGS_LOCATION/apps/panel/$@.xml
+}
+
+# Save the current configuration for Gnome-panel.
+#
+# Arguments
+#   1 (required) the name of the config to save. The file will be saved
+#                at $GNOME_CONFIGS_LOCATION/apps/panel/<name>.xml
+gnome-panel-save()
+{
+    gconftool --dump apps/panel > $GNOME_CONFIGS_LOCATION/apps/panel/$@.xml
+}
