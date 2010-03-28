849a5a666d1334cdd5718be74a80ad3b71336beb
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-03-29 00:16:26 +0200

    Fix buggy prompt

--- bash/configs/Prompt.sh
+++ bash/configs/Prompt.sh
@@ -5,116 +5,98 @@
 #
 # Dependencies
 #   * bash_profile
-#   * Commands: svn, git, readlink, awk.
+#   * Commands: svn, git, sed.
 #   * Configs: Functions.
 # ------------------------------------------------------------------------------
 
 
 # ------------------------------------------------------------------------------
-# Tweak prompt
+# Constants
 # ------------------------------------------------------------------------------
 
-# Calculate prompt elements
-function bash_prompt_command() {
+# Maximum length of the path displayed in the prompt.
+export PROMPT_PATH_MAX_LENGTH=30
 
-    # Truncate pwd
-    # How many characters of the $PWD should be kept
-    local pwdmaxlen=30
-    # Indicate that there has been dir truncation
-    local trunc_symbol="..."
-    local dir=${PWD##*/}
-    pwdmaxlen=$(( ( pwdmaxlen < ${#dir} ) ? ${#dir} : pwdmaxlen ))
-    NEW_PWD=${PWD/#$HOME/\~}
-    local pwdoffset=$(( ${#NEW_PWD} - pwdmaxlen ))
-    if [ ${pwdoffset} -gt "0" ]
-    then
-        NEW_PWD=${NEW_PWD:$pwdoffset:$pwdmaxlen}
-        NEW_PWD=${trunc_symbol}/${NEW_PWD#*/}
-    fi
-    
-    # Display clock only if it has changed
-    MINUTE=$(date +%M)
-    if [ $(($MINUTE - $OLD_MINUTE)) -ne "0" ]
-    then
-        CLOCK="$(date +%H:%M)|"
-    else
-        CLOCK=""
-    fi   
-    OLD_MINUTE=$MINUTE
-    
-    # Versionned directories features
-    local vcs base_dir sub_dir ref
-    sub_dir() {
-        local sub_dir
-        sub_dir=$(readlink -f "${PWD}")
-        sub_dir=${sub_dir#$1}
-        echo ${sub_dir#/}
-    }
-    function git_dir() {
-        base_dir=$(git-rev-parse --show-cdup 2>/dev/null) || return 1
-        base_dir=$(readlink -f "$base_dir/..")
-        sub_dir=$(git-rev-parse --show-prefix)
-        sub_dir=${sub_dir%/}
-        ref=$(git-symbolic-ref -q HEAD || git-name-rev --name-only HEAD 2>/dev/null)
-        ref=${ref#refs/heads/}
-        vcs="git"
-    }
-    function svn_dir() {
-    [ -d ".svn" ] || return 1
-        base_dir="."
-        while [ -d "$base_dir/../.svn" ]; do base_dir="$base_dir/.."; done
-        base_dir=$(readlink -f "$base_dir")
-        sub_dir=$(sub_dir "${base_dir}")
-        ref=$(svn info "$base_dir" | awk '/^URL/ { sub(".*/","",$0); r=$0 } /^Revision/ { sub("[^0-9]*","",$0); print r":"$0 }')
-        vcs="svn"
-    }
-    function svk_dir() {
-        [ -f ~/.svk/config ] || return 1
-        base_dir=$(awk '/: *$/ { sub(/^ */,"",$0); sub(/: *$/,"",$0); if (match("'${PWD}'", $0?(/|$)")) { print $0; d=1; } } /depotpath/ && d == 1 { sub(".*/","",$0); r=$0 } /revision/ && d == 1 { print r ":" $2; exit 1 }' ~/.svk/config) && return 1
-        ref=${base_dir##*
-        }
-        base_dir=${base_dir%%
-        *}
-        sub_dir=$(sub_dir "${base_dir}")
-        vcs="svk"
-    }
-    function hg_dir() {
-        base_dir="."
-        while [ ! -d "$base_dir/.hg" ]; do base_dir="$base_dir/.."; [ $(readlink -f "${base_dir}") = "/" ] && return 1; done
-        base_dir=$(readlink -f "$base_dir")
-        sub_dir=$(sub_dir "${base_dir}")
-        ref=$(< "${base_dir}/.hg/branch")
-        vcs="hg"
-    }
-    git_dir ||
-    svn_dir ||
-    #svk_dir ||
-    #hg_dir ||
-    base_dir="$PWD"
-    if [ -n "$vcs" ]; then
-    __vcs_prefix="[$vcs]"
-    __vcs_base_dir="$NEW_PWD"
-    __vcs_ref="[$ref]"
-    __vcs_sub_dir="${sub_dir}"
-    else
-    __vcs_prefix=""
-    __vcs_base_dir="${NEW_PWD}"
-    __vcs_ref=""
-    __vcs_sub_dir=""
-    fi
+# Symbol to display before the path to mark it has been truncated.
+export PROMPT_PATH_TRUNCATION_SYMBOL="..."
 
-# Set Window title
-set-terminal-title ${NEW_PWD}
 
+# ------------------------------------------------------------------------------
+# Functions
+# ------------------------------------------------------------------------------
+
+# Print infos if the directory is a VCS working copy.
+#
+# Arguments
+#   1 (required) the name of the VCS.
+#   2 (required) the info to print.
+#   3 (required) must be 1 if the working has uncommited changes.
+_print_vcs_info()
+{
+  local result=$TEXT_WHITE$BOLD
+  if [[ $3 -eq 1 ]]; then
+    result=$BACKGROUND_BLUE$result
+  else
+    result=$BACKGROUND_GREEN$result
+  fi
+  result=$result$1"|"$2$RESET_FORMATTING
+  echo $result
 }
 
-# Construct new prompt
-function bash_prompt() {
-    PS1="${debian_chroot:+($debian_chroot)}\[$BACKGROUND_BLACK\]\$CLOCK\[$BACKGROUND_GREEN\]\[$BOLD\]\$__vcs_prefix\[$RESET_FORMATTING\]\[$BACKGROUND_BLACK\]\$NEW_PWD\[$RESET_FORMATTING\]\$ "
+# Print infos if the directory is a Git working copy.
+_print_git_info()
+{
+  # Get the status from Git
+  local status=$( git status 2>/dev/null )
+  
+  # If we are in a versionned directory
+  if [[ -n $status ]]; then
+    _print_vcs_info "GIT" \
+                     "$(git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* //')" \
+                     "$( [[ "$status" == *'working directory clean'* ]] || echo 1 )"
+  fi
 }
 
-# Build new prompt
-OLD_MINUTE="-1"
-PROMPT_COMMAND=bash_prompt_command
-bash_prompt
-unset bash_prompt
+# Print infos if the directory is a Subversion working copy.
+_print_svn_info()
+{
+  # Get the version from SVN
+  local version=$( svnversion 2>/dev/null )
+  
+  # If we are in a versionned directory
+  if [[ $version != 'export'* ]]; then
+    local revision=$( echo $version | sed -e 's/[^0-9]//g' )
+	_print_vcs_info "SVN" \
+	                 "r$revision" \
+	                 "$( test $version == $revision; echo $? )"
+  fi
+}
+
+# Print the current path truncated.
+# See $PROMPT_PATH* constants.
+_print_path()
+{
+  # Truncate the path
+  local dir=${PWD##*/}
+  pwdmaxlen=$(( ( $PROMPT_PATH_MAX_LENGTH < ${#dir} ) ? ${#dir} : $PROMPT_PATH_MAX_LENGTH ))
+  result=${PWD/#$HOME/\~}
+  local pwdoffset=$(( ${#result} - pwdmaxlen ))
+  if [ ${pwdoffset} -gt "0" ]; then
+    result=${result:$pwdoffset:$pwdmaxlen}
+    result=$PROMPT_PATH_TRUNCATION_SYMBOL/${result#*/}
+  fi
+  
+  # While here, update the terminal title
+  set-terminal-title $result
+  
+  # Print the result
+  echo $result
+}
+
+
+# ------------------------------------------------------------------------------
+# Prompt override
+# ------------------------------------------------------------------------------
+
+# Define the custom prompt.
+PS1="\[$(_print_svn_info)\]\[$(_print_git_info)\]\[$(_print_path)\] \[$BOLD\]\$\[$RESET_FORMATTING\] "
