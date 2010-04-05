1647be671dc5774397e7f1a4088ffce6c8a28850
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-04-06 00:22:39 +0200

    Improve proxy management

--- shell/configs/Proxy environment.sh
+++ shell/configs/Proxy environment.sh
@@ -12,10 +12,31 @@
 #   none.
 # ------------------------------------------------------------------------------
 
-# Http proxy
-export http_proxy="http://${PROXY_HOST}:${PROXY_PORT}"
 
-# Execution options
-export JAVA_OPTS="${JAVA_OPTS} -Dhttp.proxyHost=${PROXY_HOST} -Dhttp.proxyPort=${PROXY_PORT} -Dhttp.proxySet=true -Dhttp.nonProxyHosts=\"${NON_PROXY_HOSTS}\""
-export MAVEN_OPTS="${MAVEN_OPTS} -Dhttp.proxyHost=${PROXY_HOST} -Dhttp.proxyPort=${PROXY_PORT} -Dhttp.proxySet=true -Dhttp.nonProxyHosts=\"${NON_PROXY_HOSTS}\""
-export ANT_OPTS="${ANT_OPTS} -Dhttp.proxyHost=${PROXY_HOST} -Dhttp.proxyPort=${PROXY_PORT} -Dhttp.proxySet=true -Dhttp.nonProxyHosts=\"${NON_PROXY_HOSTS}\""
+# Print a summary of the proxy-related environment variables.
+function proxy-check ()
+{
+  echo "`set_color -o blue`\$http_proxy`set_color normal` $http_proxy"
+  echo "`set_color -o blue`\$PROXY_HOST`set_color normal` $PROXY_HOST"
+  echo "`set_color -o blue`\$PROXY_PORT`set_color normal` $PROXY_PORT"
+  echo "`set_color -o blue`\$NON_PROXY_HOSTS`set_color normal` $NON_PROXY_HOSTS"
+}
+
+
+# Update the environment variables
+if [[ -n $PROXY_HOST ]]; then
+  
+  # Assign a default value to the port if not set
+  if [[ -z $PROXY_PORT ]]; then
+    export PROXY_PORT=80
+  fi
+  
+  # Default proxy variable
+  export http_proxy="http://${PROXY_HOST}:${PROXY_PORT:-80}"
+
+  # Execution options
+  export JAVA_OPTS="${JAVA_OPTS} -Dhttp.proxyHost=${PROXY_HOST} -Dhttp.proxyPort=${PROXY_PORT} -Dhttp.proxySet=true -Dhttp.nonProxyHosts=\"${NON_PROXY_HOSTS}\""
+  export MAVEN_OPTS="${MAVEN_OPTS} -Dhttp.proxyHost=${PROXY_HOST} -Dhttp.proxyPort=${PROXY_PORT} -Dhttp.proxySet=true -Dhttp.nonProxyHosts=\"${NON_PROXY_HOSTS}\""
+  export ANT_OPTS="${ANT_OPTS} -Dhttp.proxyHost=${PROXY_HOST} -Dhttp.proxyPort=${PROXY_PORT} -Dhttp.proxySet=true -Dhttp.nonProxyHosts=\"${NON_PROXY_HOSTS}\""
+
+fi
