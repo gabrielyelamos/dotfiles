4a8a62d41ddbdaa06c8e191fe83f3ac17b8c4ce0
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-04-05 20:28:02 +0200

    Remove old bash folder

--- bash/header/print-header-line.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# ------------------------------------------------------------------------------
-# Description
-#   Prints a line with the user and host names.
-# ------------------------------------------------------------------------------
-
-# Add the user and host names
-echo -e "${TEXT_BLUE}${BOLD}❮$USER@$HOSTNAME❯${RESET_FORMATTING}"
