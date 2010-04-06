28020053b07e4d337493f60e425b66962dc164fe
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-04-06 22:39:55 +0200

    Start Zsh options

--- shell/configs/Zsh settings.sh
+++ shell/configs/Zsh settings.sh
@@ -6,18 +6,114 @@
 
 if [[ $SHELL_TYPE == 'zsh' ]]; then
 
-  zstyle ':completion:*' completer _list _oldlist _expand _complete _ignored _match _correct _approximate _prefix
-  zstyle ':completion:*' max-errors 1 numeric
-  zstyle :compinstall filename '/home/nicoulaj/.zshrc'
-
+  # Activate auto-completion
   autoload -Uz compinit
   compinit
-
-  HISTFILE=~/.histfile
-  HISTSIZE=10000
-  SAVEHIST=20000
-  setopt appendhistory autocd extendedglob nomatch notify
-  unsetopt beep
+  
+  # Use Emacs line editing mode
   bindkey -e
+
+  # ----------------------------------------------------------------------------
+  # Options
+  # ----------------------------------------------------------------------------
+
+  # Changing Directories
+
+  # If a command is issued that can't be executed as a normal command, and the
+  # command is the name of a directory, perform the cd command to that directory.
+  setopt AUTO_CD
+
+  # Make cd push the old directory onto the directory stack.
+  setopt AUTO_PUSHD
+
+  # If the argument to a cd command (or an implied cd with the AUTO_CD option set)
+  # is not a directory, and does not begin with a slash, try to expand the expression
+  # as if it were preceded by a `~' (see Filename Expansion).
+  setopt CDABLE_VARS
+
+  # When changing to a directory containing a path segment `..' which would otherwise
+  # be treated as canceling the previous segment in the path (in other words, `foo/..'
+  # would be removed from the path, or if `..' is the first part of the path, the last
+  # part of $PWD would be deleted), instead resolve the path to the physical directory.
+  # This option is overridden by CHASE_LINKS.
+  # For example, suppose /foo/bar is a link to the directory /alt/rod. Without this
+  # option set, `cd /foo/bar/..' changes to /foo; with it set, it changes to /alt. The
+  # same applies if the current directory is /foo/bar and `cd ..' is used. Note that
+  # all other symbolic links in the path will also be resolved.  
+  # setopt CHASE_DOTS
+
+  # Resolve symbolic links to their true values when changing directory. This also has
+  # the effect of CHASE_DOTS, i.e. a `..' path segment will be treated as referring to
+  # the physical parent, even if the preceding path segment is a symbolic link.
+  setopt CHASE_LINKS
+
+  # Don't push multiple copies of the same directory onto the directory stack.
+  setopt PUSHD_IGNORE_DUPS
+
+  # Exchanges the meanings of `+' and `-' when used with a number to specify a
+  # directory in the stack.
+  # setopt PUSHD_MINUS
+
+  # Do not print the directory stack after pushd or popd.
+  # setopt PUSHD_SILENT
+
+  # Have pushd with no arguments act like `pushd $HOME'.
+  setopt PUSHD_TO_HOME
+
+  
+  # Completion
+
+  # TODO
+
+  
+  # Expansion and Globbing
+
+  # TODO
+  
+  
+  # History
+
+  # TODO
+  
+  
+  # Initialisation
+
+  # TODO
+  
+  
+  # Input/Output
+
+  # TODO
+  
+  
+  # Job Control
+
+  # TODO
+  
+  
+  # Prompting
+
+  # TODO
+  
+  
+  # Scripts and Functions
+
+  # TODO
+  
+  
+  # Shell Emulation
+
+  # TODO
+  
+  
+  # Shell State
+
+  # TODO
+  
+  
+  # Zle
+
+  # TODO
+  
 
 fi
