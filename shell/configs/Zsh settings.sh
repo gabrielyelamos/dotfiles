0a082657b7776b7b24f305e2577c2adaad60076f
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-04-07 22:42:47 +0200

    Add Zsh options for history

--- shell/configs/Zsh settings.sh
+++ shell/configs/Zsh settings.sh
@@ -315,8 +315,109 @@ if [[ $SHELL_TYPE == 'zsh' ]]; then
   
   # History
 
-  # TODO
-  
+  # If this is set, zsh sessions will append their history list to the history file, rather than
+  # replace it. Thus, multiple parallel zsh sessions will all have the new entries from their
+  # history lists added to the history file, in the order that they exit. The file will still
+  # be periodically re-written to trim it when the number of lines grows 20% beyond the value
+  # specified by $SAVEHIST (see also the HIST_SAVE_BY_COPY option).
+  setopt APPEND_HISTORY
+
+  # Perform textual history expansion, csh-style, treating the character `!' specially.
+  setopt BANG_HIST
+
+  # Save each command's beginning timestamp (in seconds since the epoch) and the duration (in
+  # seconds) to the history file. The format of this prefixed data is:
+  # `:<beginning time>:<elapsed seconds>:<command>'.
+  setopt EXTENDED_HISTORY
+
+  # Add `|' to output redirections in the history. This allows history references to clobber
+  # files even when CLOBBER is unset.
+  unsetopt HIST_ALLOW_CLOBBER
+
+  # Beep when an attempt is made to access a history entry which isn't there.
+  unsetopt HIST_BEEP
+
+  # If the internal history needs to be trimmed to add the current command line, setting this
+  # option will cause the oldest history event that has a duplicate to be lost before losing a
+  # unique event from the list. You should be sure to set the value of HISTSIZE to a larger
+  # number than SAVEHIST in order to give you some room for the duplicated events, otherwise
+  # this option will behave just like HIST_IGNORE_ALL_DUPS once the history fills up with
+  # unique events.
+  setopt HIST_EXPIRE_DUPS_FIRST
+
+  # When writing out the history file, by default zsh uses ad-hoc file locking to avoid known
+  # problems with locking on some operating systems. With this option locking is done by means
+  # of the system's fcntl call, where this method is available. On recent operating systems this
+  # may provide better performance, in particular avoiding history corruption when files are
+  # stored on NFS.
+  unsetopt HIST_FCNTL_LOCK
+
+  # When searching for history entries in the line editor, do not display duplicates of a line
+  # previously found, even if the duplicates are not contiguous.
+  setopt HIST_FIND_NO_DUPS
+
+  # If a new command line being added to the history list duplicates an older one, the older
+  # command is removed from the list (even if it is not the previous event).
+  setopt HIST_IGNORE_ALL_DUPS
+
+  # Do not enter command lines into the history list if they are duplicates of the previous event.
+  setopt HIST_IGNORE_DUPS
+
+  # Remove command lines from the history list when the first character on the line is a space,
+  # or when one of the expanded aliases contains a leading space. Note that the command lingers
+  # in the internal history until the next command is entered before it vanishes, allowing you
+  # to briefly reuse or edit the line. If you want to make it vanish right away without entering
+  # another command, type a space and press return.
+  setopt HIST_IGNORE_SPACE
+
+  # Remove function definitions from the history list. Note that the function lingers in the
+  # internal history until the next command is entered before it vanishes, allowing you to briefly
+  # reuse or edit the definition.
+  unsetopt HIST_NO_FUNCTIONS
+
+  # Remove the history (fc -l) command from the history list when invoked. Note that the command
+  # lingers in the internal history until the next command is entered before it vanishes, allowing
+  # you to briefly reuse or edit the line.
+  setopt HIST_NO_STORE
+
+  # Remove superfluous blanks from each command line being added to the history list.
+  setopt HIST_REDUCE_BLANKS
+
+  # When the history file is re-written, we normally write out a copy of the file named $HISTFILE.new
+  # and then rename it over the old one. However, if this option is unset, we instead truncate the old
+  # history file and write out the new version in-place. If one of the history-appending options is
+  # enabled, this option only has an effect when the enlarged history file needs to be re-written to
+  # trim it down to size. Disable this only if you have special needs, as doing so makes it possible
+  # to lose history entries if zsh gets interrupted during the save.
+  # When writing out a copy of the history file, zsh preserves the old file's permissions and group
+  # information, but will refuse to write out a new file if it would change the history file's owner.
+  setopt HIST_SAVE_BY_COPY
+
+  # When writing out the history file, older commands that duplicate newer ones are omitted.
+  setopt HIST_SAVE_NO_DUPS
+
+  # Whenever the user enters a line with history expansion, don't execute the line directly; instead,
+  # perform history expansion and reload the line into the editing buffer.
+  setopt HIST_VERIFY
+
+  # This options works like APPEND_HISTORY except that new history lines are added to the $HISTFILE
+  # incrementally (as soon as they are entered), rather than waiting until the shell exits. The file will
+  # still be periodically re-written to trim it when the number of lines grows 20% beyond the value
+  # specified by $SAVEHIST (see also the HIST_SAVE_BY_COPY option).
+  setopt INC_APPEND_HISTORY
+
+  # This option both imports new commands from the history file, and also causes your typed commands to
+  # be appended to the history file (the latter is like specifying INC_APPEND_HISTORY). The history lines
+  # are also output with timestamps ala EXTENDED_HISTORY (which makes it easier to find the spot where we
+  # left off reading the file after it gets re-written).
+  # By default, history movement commands visit the imported lines as well as the local lines, but you can
+  # toggle this on and off with the set-local-history zle binding. It is also possible to create a zle
+  # widget that will make some commands ignore imported commands, and some include them.
+  # If you find that you want more control over when commands get imported, you may wish to turn
+  # SHARE_HISTORY off, INC_APPEND_HISTORY on, and then manually import commands whenever you need them
+  # using `fc -RI'.
+  setopt SHARE_HISTORY
+
   
   # Initialisation
 
