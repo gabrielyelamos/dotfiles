c1b4e8c48060cda0964dae715e096ef492fe5662
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-05-08 19:34:06 +0200

    Add settings for Debian packaging

--- .shell.d/configs/enabled/200_Settings.sh
+++ .shell.d/configs/enabled/200_Settings.sh
@@ -7,3 +7,8 @@
 
 # Add some directories to $PATH.
 export PATH=$HOME/bin:$PATH
+
+# Constants used for Debian packaging.
+export DEBFULLNAME='Julien Nicoulaud'
+export DEBEMAIL='julien.nicoulaud@gmail.com'
+export GPGKEY='A20BF77D'
