18489df6b6c8a4f7a217219c117c44ae47159501
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-05-20 20:11:13 +0200

    Add proxies

---
+++ zsh/conf.d.disabled/400_Proxy Bull ecprox.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# ------------------------------------------------------------------------------
+# Description
+#   Proxy settings.
+# ------------------------------------------------------------------------------
+
+export PROXY_HOST="ecprox.bull.fr"
+export PROXY_PORT="8080"
+export NON_PROXY_HOSTS="localhost|*.frec.bull.fr"
---
+++ zsh/conf.d.disabled/400_Proxy Bull pxlvb.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# ------------------------------------------------------------------------------
+# Description
+#   Proxy settings.
+# ------------------------------------------------------------------------------
+
+export PROXY_HOST="pxlvb.frec.bull.fr"
+export PROXY_PORT="8080"
+export NON_PROXY_HOSTS="localhost|*.frec.bull.fr|192.168.*"
