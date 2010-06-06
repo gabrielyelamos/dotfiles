3493fd48b631a05bba029978a35c4544b739bcb7
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-06-06 22:37:52 +0200

    Start customizing MIME types handling

---
+++ zsh/conf.d/350_MIME types.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+# ------------------------------------------------------------------------------
+# Description
+#   Shell MIME types handling settings.
+# ------------------------------------------------------------------------------
+
+autoload -U zsh-mime-setup
+autoload -U zsh-mime-handler
+zsh-mime-setup
+
+autoload -U pick-web-browser
+zstyle ':mime:*' x-browsers google-chrome firefox elinks
+alias -s html=pick-web-browser
