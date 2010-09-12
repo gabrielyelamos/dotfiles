8ecae348a0a20960c0d6ce6ea44a36768444ba6e
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-09-12 23:20:23 +0200

    Completion improvements

--- zsh/conf.d/350_Completion.sh
+++ zsh/conf.d/350_Completion.sh
@@ -37,9 +37,23 @@ bashcompinit
 autoload -Uz compinit
 compinit
 
-zstyle ':completion:*' accept-exact '*(N)'
+# Use cache
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path ~/.zsh_completion_cache
+
+# Ignore VCS directories
+zstyle ':completion:*:(all-|)files' ignored-patterns '(|*/).svn'
+zstyle ':completion:*:cd:*' ignored-patterns '(*/).svn'
+
+# Ignore completion functions for commands I don’t have
+zstyle ':completion:*:functions' ignored-patterns '_*'
+
+# cd style
+zstyle ':completion:*:cd:*' ignore-parents parent pwd # cd never selects the parent directory (e.g.: cd ../<TAB>)
+zstyle ':completion:*:*:cd:*' tag-order local-directories path-directories
+
+
+zstyle ':completion:*' accept-exact '*(N)'
 zstyle ':completion:*' file-sort name
 zstyle ':completion:*' list-colors ${(s.:.)ZLS_COLORS}
 zstyle ':completion:*' group-name ''
@@ -47,6 +61,7 @@ zstyle ':completion:*' menu select=2
 zstyle ':completion:*' verbose true
 zstyle ':completion:*' squeeze-slashes true
 
+# kill style
 zstyle ':completion:*:*:kill:*' command 'ps -a -w -w -u $USER -o pid,cmd --sort=-pid'
 zstyle ':completion:*:*:kill:*' menu yes select
 zstyle ':completion:*:*:kill:*' force-list always
@@ -54,9 +69,18 @@ zstyle ':completion:*:*:kill:*:processes' list-colors "=(#b) #([0-9]#)*=39=32"
 
 zstyle ':completion:*:rm:*' ignore-line yes
 zstyle ':completion:*:mv:*' ignore-line yes
-zstyle ':completion:*:cp:*' ignore-line yes 
+zstyle ':completion:*:cp:*' ignore-line yes
 
-zstyle ':completion:*:*:cd:*' tag-order local-directories path-directories
+# Rationalize dots (allows to cd ...../)
+rationalise-dot() {
+  if [[ $LBUFFER = *.. ]]; then
+    LBUFFER+=/..
+  else
+    LBUFFER+=.
+  fi
+}
+zle -N rationalise-dot
+bindkey . rationalise-dot
 
 
 # ------------------------------------------------------------------------------
