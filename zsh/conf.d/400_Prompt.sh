0df6da20a2aa20f89839c92912e213f257b9caa3
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-09-20 23:04:58 +0200

    Finalize prompt

--- zsh/conf.d/400_Prompt.sh
+++ zsh/conf.d/400_Prompt.sh
@@ -5,17 +5,15 @@
 
 autoload -Uz vcs_info
 
-FMT_BRANCH="%{$fg_bold[grey]%}%r/%s/%b%{$reset_color%} %{$fg_bold[grey]%}%u%c%{$reset_color%}"
-FMT_ACTION="%{$fg_bold[grey]%}(%a)%{$reset_color%}"
 zstyle ':vcs_info:*' enable hg bzr git
 zstyle ':vcs_info:*:*' check-for-changes true
 zstyle ':vcs_info:*:*' unstagedstr '!'
 zstyle ':vcs_info:*:*' stagedstr '+'
-zstyle ':vcs_info:*:*' actionformats "%S" "${FMT_BRANCH} ${FMT_ACTION}"
-zstyle ':vcs_info:*:*' formats "%S" "${FMT_BRANCH}"
-zstyle ':vcs_info:*:*' nvcsformats "%2~" ""
+zstyle ':vcs_info:*:*' actionformats "%S" "%r/%s/%b %u%c (%a)"
+zstyle ':vcs_info:*:*' formats "%S" "%r/%s/%b %u%c"
+zstyle ':vcs_info:*:*' nvcsformats "%30<..<%~%<<" ""
 
-PROMPT="${SSH_TTY:+%{$fg[green]%\}[%n@%m]}%{$reset_color%}%(0?.%{$fg_bold[green]%}.%{$fg[red]%})%20<..<"'${vcs_info_msg_0_%%.}'"%<<❯%{$reset_color%} "
-RPROMPT='$vcs_info_msg_1_'"%{$reset_color%}"
+PROMPT="${SSH_TTY:+%{$fg[green]%\}[%n@%m]}%{$reset_color%}%(0?.%{$fg_bold[green]%}.%{$fg[red]%})%30<..<"'${vcs_info_msg_0_%%.}'"%<<%(!.❯❯❯.❯)%{$reset_color%} "
+RPROMPT="%{$fg_bold[grey]%}"'$vcs_info_msg_1_'"%{$reset_color%}"
 
 precmd_functions+=vcs_info
