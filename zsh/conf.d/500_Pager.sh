b632f1d11297bf010542ddda1cadf42e56b28593
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-09-21 23:52:42 +0200

    Fix less options

--- zsh/conf.d/500_Pager.sh
+++ zsh/conf.d/500_Pager.sh
@@ -5,13 +5,14 @@
 
 
 # Settings for less.
-export LESS="--RAW-CONTROL-CHARS --long-prompt --status-column --HILITE-UNREAD --ignore-case --tabs=2 --shift 5 --tilde"
+export LESS="--long-prompt --status-column --HILITE-UNREAD --ignore-case --tabs=2 --shift 5 --tilde"
 export LESSCHARSET=utf-8
 export LESSHISTFILE="$HOME/.lesshst"
 export LESSHISTSIZE=100
 
 # If source-highlight available, else fall back to lesspipe.
 if [[ -f /usr/share/source-highlight/src-hilite-lesspipe.sh ]]; then
+  export LESS="$LESS --RAW-CONTROL-CHARS"
   export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
 else
   [[ -x /usr/bin/lesspipe ]] && eval $(lesspipe)
