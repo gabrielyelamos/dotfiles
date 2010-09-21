b53022ddc7c402afe8bbd97fb31b501f9cf54b60
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-09-21 18:33:08 +0200

    Comments

--- zsh/conf.d/500_Less.sh
+++ zsh/conf.d/500_Less.sh
@@ -4,10 +4,12 @@
 # ------------------------------------------------------------------------------
 
 
-# Settings for less
+# Settings for less.
 export LESS="-MWi -x2 --shift 5"
 export LESSCHARSET=utf-8
 export LESSHISTFILE="-"
+
+# Colors for less.
 export LESS_TERMCAP_mb=$'\E[01;31m'
 export LESS_TERMCAP_md=$'\E[01;31m'
 export LESS_TERMCAP_me=$'\E[0m'
@@ -15,4 +17,6 @@ export LESS_TERMCAP_se=$'\E[0m'
 export LESS_TERMCAP_so=$'\E[01;44;33m'                                 
 export LESS_TERMCAP_ue=$'\E[0m'
 export LESS_TERMCAP_us=$'\E[01;32m'
+
+# Lesspipe for browsing archives.
 [[ -x /usr/bin/lesspipe ]] && eval $(lesspipe)
