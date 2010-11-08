a9e36f4e36c51b136df8fe5d4494a275509be617
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-11-08 13:30:16 +0100

    Add a conf for Android dev

---
+++ zsh/conf.d/700_Android.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+# ------------------------------------------------------------------------------
+# Settings for Android development.
+# ------------------------------------------------------------------------------
+
+export ANDROID_HOME=/opt/android/android-sdk-linux_x86
+export PATH=$ANDROID_HOME/tools:$PATH
