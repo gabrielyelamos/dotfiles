208973618a564220702f27b0641f9c9197aca871
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-11-20 18:31:36 +0100

    Improve completion ignored patterns

--- zsh/conf.d/300_Completion.sh
+++ zsh/conf.d/300_Completion.sh
@@ -31,6 +31,20 @@ zstyle ':completion:*:cd:*' ignored-patterns '(*/)(.svn|.git|.hg)'
 # Ignore completion functions for commands I don’t have
 zstyle ':completion:*:functions' ignored-patterns '_*'
 
+# Don't complete uninteresting users
+zstyle ':completion:*:*:*:users' ignored-patterns \
+        adm amanda apache avahi beaglidx bin cacti canna clamav daemon \
+        dbus distcache dovecot fax ftp games gdm gkrellmd gopher \
+        hacluster haldaemon halt hsqldb ident junkbust ldap lp mail \
+        mailman mailnull mldonkey mysql nagios \
+        named netdump news nfsnobody nobody nscd ntp nut nx openvpn \
+        operator pcap postfix postgres privoxy pulse pvm quagga radvd \
+        rpc rpcuser rpm shutdown squid sshd sync uucp vcsa xfs www-data \
+        avahi-autoipd
+
+# Show ignored patterns if needed.
+zstyle '*' single-ignored show
+
 # cd style
 zstyle ':completion:*:cd:*' ignore-parents parent pwd # cd never selects the parent directory (e.g.: cd ../<TAB>)
 zstyle ':completion:*:*:cd:*' tag-order local-directories path-directories
