1049e7e1d4406fad51e70e5ac29acff82d559ada
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-12-04 17:40:56 +0100

    Use terminfo instead of raw key sequence

--- zsh/conf.d/300_ZLE.sh
+++ zsh/conf.d/300_ZLE.sh
@@ -18,7 +18,7 @@ export WORDCHARS=''
 # Custom ZLE Widget to bind F5 => 'source ~/.zshrc'
 _refresh() { source ~/.zshrc }
 zle -N _refresh
-bindkey '^[[15~' _refresh
+bindkey $terminfo[kf5] _refresh
 
 # Perform history expansion on space
 bindkey ' ' magic-space
