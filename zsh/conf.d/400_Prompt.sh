d5cb1a5284856a5d05cff5069b0f11957408f3c9
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-12-04 23:15:22 +0100

    Make prompt VCS info simpler

--- zsh/conf.d/400_Prompt.sh
+++ zsh/conf.d/400_Prompt.sh
@@ -5,28 +5,24 @@
 
 # Customizable parameters.
 PROMPT_PATH_MAX_LENGTH=30
-PROMPT_DEFAULT_END=❯
-PROMPT_ROOT_END=❯❯❯
 PROMPT_SUCCESS_COLOR=$FG[71]
 PROMPT_FAILURE_COLOR=$FG[124]
 PROMPT_VCS_INFO_COLOR=$FG[darkgrey]
+PROMPT_VCS_CHANGES_COLOR=$FG[172]
 
-# Load required modules.
-autoload -U add-zsh-hook
+# Load required modules and register hook.
 autoload -Uz vcs_info
-
-# Add hook for calling vcs_info before each command.
+autoload -U add-zsh-hook
 add-zsh-hook precmd vcs_info
 
 # Set vcs_info parameters.
-zstyle ':vcs_info:*' enable hg bzr git
+zstyle ':vcs_info:*' enable hg bzr svn git
 zstyle ':vcs_info:*:*' check-for-changes true # Can be slow on big repos.
-zstyle ':vcs_info:*:*' unstagedstr '!'
-zstyle ':vcs_info:*:*' stagedstr '+'
-zstyle ':vcs_info:*:*' actionformats "%S" "%r/%s/%b %u%c (%a)"
-zstyle ':vcs_info:*:*' formats "%S" "%r/%s/%b %u%c"
-zstyle ':vcs_info:*:*' nvcsformats "%~" ""
+zstyle ':vcs_info:*:*' unstagedstr "●"
+zstyle ':vcs_info:*:*' actionformats "%S" "%r/%s/%b (%a)" "%u"
+zstyle ':vcs_info:*:*' formats "%S" "%r/%s/%b" "%u"
+zstyle ':vcs_info:*:*' nvcsformats "%~" "" ""
 
 # Define prompts.
-PROMPT="%(0?.%{$PROMPT_SUCCESS_COLOR%}.%{$PROMPT_FAILURE_COLOR%})${SSH_TTY:+[%n@%m]}%{$FX[bold]%}%$PROMPT_PATH_MAX_LENGTH<..<"'${vcs_info_msg_0_%%.}'"%<<%(!.$PROMPT_ROOT_END.$PROMPT_DEFAULT_END)%{$FX[no-bold]%}%{$FX[reset]%} "
-RPROMPT="%{$PROMPT_VCS_INFO_COLOR%}"'$vcs_info_msg_1_'"%{$FX[reset]%}"
+PROMPT="%(0?.%{$PROMPT_SUCCESS_COLOR%}.%{$PROMPT_FAILURE_COLOR%})${SSH_TTY:+[%n@%m]}%{$FX[bold]%}%$PROMPT_PATH_MAX_LENGTH<..<"'${vcs_info_msg_0_%%.}'"%<<%(!.❯❯❯.❯)%{$FX[no-bold]%}%{$FX[reset]%} "
+RPROMPT="%{$PROMPT_VCS_INFO_COLOR%}"'$vcs_info_msg_1_'"%{$FX[reset]%} %{$PROMPT_VCS_CHANGES_COLOR%}"'$vcs_info_msg_2_'"%{$FX[reset]%}"
