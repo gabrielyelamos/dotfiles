d12ae3c983c4a845f21c04072037f65cce64c8b0
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-12-31 01:52:01 +0100

    Add conf for grep

---
+++ zsh/conf.d/500_Grep.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+# ------------------------------------------------------------------------------
+# Settings for Grep.
+# ------------------------------------------------------------------------------
+
+
+# Ignore SCM directories
+if ! $(grep --exclude-dir 2> /dev/null); then
+    GREP_OPTIONS="--color --exclude-dir=.svn --exclude=\*.pyc --exclude-dir=.hg --exclude-dir=.bzr --exclude-dir=.git"
+else
+    GREP_OPTIONS="--color --exclude=\*.svn\* --exclude=\*.pyc --exclude=\*.hg\* --exclude=\*.bzr\* --exclude=\*.git\*"
+fi
+export GREP_OPTIONS
