e8b9722ec63cc0ab9008e99f4120ab330768b990
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2011-11-04 16:15:47 +0100

    zsh: remove Android conf

--- zsh/zshrc.d/30_Android.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-# vim: ft=zsh sw=2 ts=2 et
-# ------------------------------------------------------------------------------
-# Settings for Android development.
-# ------------------------------------------------------------------------------
-
-ANDROID_HOME=/opt/android/android-sdk-linux_x86
-[[ -d $ANDROID_HOME ]] && PATH=$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH
-
